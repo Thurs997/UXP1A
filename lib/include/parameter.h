34bdb48cea9637983556cc4d26de6686d8b4ee68
Thurs997 <lukasz.k92@gmail.com>
2014-06-01 21:02:49 +0200

    Nawet nie wiem co

--- lib/include/parameter.h
+++ lib/include/parameter.h
@@ -8,6 +8,30 @@
 #ifndef PARAMETER_H_
 #define PARAMETER_H_
 
+class ParameterBase
+{
+public:
+    virtual ~ParameterBase() {}
+    template<class T> const T& get() const; //to be implimented after Parameter
+    template<class T, class U> void setValue(const U& rhs); //to be implimented after Parameter
+};
+
+template <typename T>
+class Parameter : public ParameterBase
+{
+public:
+    Parameter(const T& rhs) :value(rhs) {}
+    const T& get() const {return value;}
+    void setValue(const T& rhs) {value=rhs;}
+private:
+    T value;
+};
+
+//Here's the trick: dynamic_cast rather than virtual
+template<class T> const T& ParameterBase::get() const
+{ return dynamic_cast<const Parameter<T>&>(*this).get(); }
+template<class T, class U> void ParameterBase::setValue(const U& rhs)
+{ return dynamic_cast<Parameter<T>&>(*this).setValue(rhs); }
 
 
 
