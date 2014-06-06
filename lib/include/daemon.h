edcd24d4a411e0a9c26a8eac9983b2bd2703c406
Thurs997 <lukasz.k92@gmail.com>
2014-06-06 02:19:41 +0200

    Zrobione Makefile, dodany demon

--- lib/include/daemon.h
+++ lib/include/daemon.h
@@ -8,8 +8,25 @@
 #ifndef DAEMON_H_
 #define DAEMON_H_
 
+#define LOCK_FILE "/tmp/lindacommd.lock"
+#define SLEEP_SEC 4
+#define LINDACOMM_TUPLE_FILE_ENV "$LINDACOMM_TUPLE_FILE"
+#define LINDACOMM_QUEUE_FILE_ENV "$LINDACOMM_QUEUE_FILE"
 
 
-
+class Daemon {
+public:
+	void daemonize();
+	void init();
+	void run();
+private:
+	char * tupleFileName;
+	char * queueFileName;
+	int runningLibInstances();
+	void defragment();
+	void defragmentTuple();
+	void defragmentQueue();
+	bool fileExists(const char * file);
+};
 
 #endif /* DAEMON_H_ */
--- lib/src/daemon.cpp
+++ lib/src/daemon.cpp
@@ -5,6 +5,99 @@
  *      Author: Łukasz Kamiński
  */
 
+#include "daemon.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <cstdlib>
+#include <thread>
+#include <chrono>
+#include <sys/stat.h>
+
+
+void Daemon::init() {
+	//get file names
+	tupleFilePath = getenv(LINDACOMM_TUPLE_FILE_ENV);
+	queueFilePath = getenv(LINDACOMM_QUEUE_FILE_ENV);
+	if(!(tupleFilePath && queueFilePath)) {
+		exit(1);
+	}
+	//check if files exist
+
+}
+
+bool Daemon::fileExists(const char* file) {
+    struct stat buf;
+    return (stat(file, &buf) == 0);
+}
+
+void Daemon::daemonize() {
+    int pid = fork();
+    if (pid < 0)
+        exit(1); // fork failed
+    if (pid > 0)
+        exit(0); // still in parent process, so quit
+
+    // in child process, obtain new session and process group at the same time
+    setsid();
+
+    int devnull = open("/dev/null", O_RDWR);
+
+    // close stdin stdout and stderr which are connected to current terminal
+    close(0);
+    close(1);
+    close(2);
+
+    // attach stdin stdout and stderr to /dev/null
+    // by dup-ing the devnull descriptor
+    dup2(devnull, 0);
+    dup2(devnull, 1);
+    dup2(devnull, 2);
+
+    // get rid of devnull
+    close(devnull);
+
+    // open a lockfile
+    int lockfd = open(LOCK_FILE, O_RDWR|O_CREAT, 0640);
+    if (lockfd < 0)
+        exit(1); // Error opening the file
+    if (lockf(lockfd, F_TLOCK, 0) < 0) // do a trylock
+        exit(0); // if already locked or error, exit (ensures a single process in the system)
+}
+
+void Daemon::run() {
+	while(runningLibInstances() > 0) {
+		std::this_thread::sleep_for(std::chrono::seconds(SLEEP_SEC));
+		defragment();
+	}
+}
+
+int Daemon::runningLibInstances() {
+	//TODO
+	//count results of 'find /tmp | grep lindacomm.pid_*'
+}
+
+void Daemon::defragment() {
+	defragmentTuples();
+	defragmentQueue();
+}
+
+void Daemon::defragmentTuple() {
+	//TODO
+}
+
+void Daemon::defragmentQueue() {
+	//TODO
+}
+
+int main() {
+	Daemon d;
+	d.daemonize();
+	d.init();
+	d.run();
+	return 0;
+}
+
+
 
 
 
